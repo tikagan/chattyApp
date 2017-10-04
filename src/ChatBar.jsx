b760893d0e06174c72790993be3d4b83e1416fe2
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-04 13:35:09 -0400

    added functions to handle change in both username and message text areas and to send messages and set a new username

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,14 +1,46 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: this.props.currentUser.name,
+      content: ''
+    }
+  }
+
   render() {
     console.log('Rendering <ChatBar />')
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)"/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input value={this.state.username} onChange={this.handleUsernameChange} onKeyPress={this.handleUsernameSend} className="chatbar-username" placeholder="Type your name and hit ENTER (Optional) "/>
+        <input value={this.state.content} onChange={this.handleMessageChange} onKeyPress={this.handleMessageSend} className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
+
+  handleMessageChange = (e) => {
+    this.setState({content: e.target.value})
+  }
+
+  handleMessageSend = (e) => {
+    if (e.key === "Enter") {
+      this.props.onMessageSend(this.state.content)
+      this.setState({content: ''})
+    }
+  }
+
+  handleUsernameChange = (e) => {
+    this.setState({username: e.target.value})
+  }
+
+  handleUsernameSend = (e) => {
+    if (e.key === "Enter") {
+      this.props.onUsernameSend(this.state.username)
+    }
+  }
+
 }
 export default ChatBar;
