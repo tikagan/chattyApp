514576a0fc12b7c26fe570ab30fc6fe5dec188a8
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-04 15:01:48 -0400

    removed hard coded incoming message simulation and added websocket connection, stored in this.socket in onMessageSend client sends server json object of the new message

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,10 @@ class App extends Component {
 
   componentDidMount() {
     console.log('componentDidMount <App />')
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket = new WebSocket ('ws://localhost:3001')
+    this.socket.onopen = function (event) {
+      console.log("connected to WebSocket Server")
+    }
   }
 
 
@@ -50,6 +45,7 @@ class App extends Component {
     }
     const messages = this.state.messages.concat(newMessage)
     this.setState({messages: messages})
+    this.socket.send(JSON.stringify(newMessage))
   }
 
   onUsernameSend = (name) => {
