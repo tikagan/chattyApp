8f142f856e80abde955a29c1c8405cb1bfe3ccb1
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-04 17:14:00 -0400

    required uuid, created broadcast function to send data to all clients, when receiving a message from client add type message, uuid to it and broadcast

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ new WebpackDevServer(webpack(config), {
 
   const express = require('express');
   const SocketServer = require('ws').Server;
+  const uuidv1 = require('uuid/v1');
 
   // Set the port to 3001
   const PORT = 3001;
@@ -33,22 +34,31 @@ new WebpackDevServer(webpack(config), {
     .use(express.static('public'))
     .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-  // Create the WebSockets server
-  const wss = new SocketServer({ server });
+    //broadcast function
 
-  // Set up a callback that will run when a client connects to the server
-  // When a client connects they are assigned a socket, represented by
-  // the ws parameter in the callback.
-  wss.on('connection', (ws) => {
-    console.log('Client connected');
+    const broadcast = (msg) => {
+      wss.clients.forEach(function each(client) {
+        client.send(JSON.stringify(msg))
+      })
+    }
 
-    ws.on('message', (message) =>{
-      console.log('recieved message: ', message)
-      const msg = JSON.parse(message)
-      msg.type = 'message'
-      ws.send(JSON.stringify(msg))
+    // Create the WebSockets server
+    const wss = new SocketServer({ server });
 
-    })
-    // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-    ws.on('close', () => console.log('Client disconnected'));
+    // Set up a callback that will run when a client connects to the server
+    // When a client connects they are assigned a socket, represented by
+    // the ws parameter in the callback.
+    wss.on('connection', (ws) => {
+      console.log('Client connected');
+
+      ws.on('message', (message) =>{
+        console.log('recieved message: ', message)
+        const msg = JSON.parse(message)
+        msg.type = 'message'
+        msg.id = uuidv1()
+        broadcast(msg)
+
+      })
+      // Set up a callback for when a client closes the socket. This usually means they closed their browser.
+      ws.on('close', () => console.log('Client disconnected'));
   });
