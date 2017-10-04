c93060b8fc9eefa7351fee930340b3ff3f8f8075
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-04 11:50:10 -0400

    moved console.log before return statement

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,15 +3,14 @@ import React, {Component} from 'react';
 class Message extends Component {
 
   render () {
+    console.log('Rendering <Message />')
     return (
-      console.log('Rendering <Message />')
       <div>
         <div className="message">
           <span className="message-username">{this.props.username}</span>
           <span className="message-content">{this.props.content}</span>
         </div>
         <div className="message system">
-
         </div>
       </div>
       )
