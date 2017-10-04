370a53849ba078960286453658bc73b1ea07029e
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-04 11:41:03 -0400

    added hardcoded currentUser and messages data, componentDidMount() function

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,33 @@ class App extends Component {
     super(props);
     //set up the default state for the application
     this.state = {
-
+      currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 1,
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          id: 2,
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
     }
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
 
   render() {
@@ -18,8 +43,8 @@ class App extends Component {
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={this.state.messages}/>
+        <ChatBar currentUser={this.state.currentUser}/>
       </div>
 
     );
