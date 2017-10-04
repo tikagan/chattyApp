2ad06c28ca131c0430b1298276ed3eb38ff78fa2
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-04 11:39:36 -0400

    implemented toRender function, passes data and renders <Message/> with props

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,11 +3,21 @@ import Message from './Message.jsx'
 
 class MessageList extends Component {
 
+   toRender (messages) {
+    return (
+    (messages).map((message) =>
+      <Message key={message.id} username={message.username} content={message.content}></Message>
+      )
+    )
+  }
+
+
+
   render() {
     return(
     <div>
       <main className="messages">
-        <Message />
+        {this.toRender(this.props.messages)}
       </main>
     </div>)
   }
