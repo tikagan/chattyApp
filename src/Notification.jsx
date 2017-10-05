615a5936d9261ade6a709c7261b55db6fb5d0158
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-05 13:29:26 -0400

    inital setup of notification component

---
+++ src/Notification.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Notification extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    console.log('Rendering <Notification />')
+
+    return (
+        <div className="message system">
+      
+        </div>
+      )
+  }
+}
+
+export default Message;
