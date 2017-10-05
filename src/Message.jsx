f45c2807df8d64aabddbb186eb2c113b2ee56587
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-05 13:25:49 -0400

    moved div .message system into it's own jsx file

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 
 class Message extends Component {
+  constructor(props) {
+    super(props)
+
+  }
 
   render () {
     console.log('Rendering <Message />')
     return (
-      <div>
         <div className="message">
           <span className="message-username">{this.props.username}</span>
           <span className="message-content">{this.props.content}</span>
         </div>
-        <div className="message system">
-        </div>
-      </div>
       )
   }
 }
