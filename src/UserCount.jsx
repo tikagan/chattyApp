32023c98a53256aa5d97871408988531f5ea1dfb
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-05 17:37:56 -0400

    inital component setup

---
+++ src/UserCount.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class UserCount extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    console.log('Rendering <Message />')
+    return (
+
+      )
+  }
+}
+
+export default UserCount;
