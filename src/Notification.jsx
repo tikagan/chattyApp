9ca62ee488d682a6fd00994e4d81986a11621381
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-05 17:37:22 -0400

    formatting changes

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -4,13 +4,13 @@ class Notification extends Component {
   constructor(props) {
     super(props)
   }
-  
+
   render () {
     console.log('Rendering <Notification />')
 
     return (
         <div className="message system">
-          <span className='notification content'>${this.props.content}</span>
+          <span className='notification content'>{this.props.content}</span>
         </div>
       )
   }
