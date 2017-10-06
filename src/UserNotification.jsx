384d6029139669e696d353bc4af59b9e29209f2a
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-06 16:28:02 -0400

    inital class setup

---
+++ src/UserNotification.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class UserNotification extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    console.log('Rendering <UserNotification />')
+
+    return (
+        <div className="user message system">
+          <span className='user notification content'> const messages = this.state.messages.concat(data)
+            this.setState({messages: messages})</span>
+        </div>
+      )
+  }
+}
+
+export default UserNotification;
