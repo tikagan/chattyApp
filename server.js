ec6c648252572716112eed3e3f0c84a0d966def1
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-06 16:24:14 -0400

    modified userJoinedNotifications and userLeftNotifications to accurately keep track of clients with assigned ids and send the id of each client that disconnects to all connected clients

--- server.js
+++ server.js
@@ -35,60 +35,83 @@ new WebpackDevServer(webpack(config), {
     .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
     //broadcast function
-    const broadcast = (msg) => {
+    const broadcast = (userCount) => {
       wss.clients.forEach(function each(client) {
-        client.send(JSON.stringify(msg))
+        client.send(JSON.stringify(userCount))
       })
     }
 
-    const userJoinedNotifications = (users) => {
-      users.id = uuidv1()
-      users.content = 'Anonymous has joined the chat.'
-      users.count ++
-      users.type = 'userJoinedNotification'
-      broadcast(users)
+    const userJoinedNotifications = (userCount, ws) => {
+      ws.id = uuidv1()
+      userCount.wsid = ws.id
+      userCount.id = uuidv1()
+      userCount.type = 'userJoinedNotification'
+      saveUser(userCount)
+      broadcast(userCount)
     }
 
-    const userLeftNotifications = (users) => {
-      users.id = uuidv1()
-      type: 'userLeftNotification'
-      users.count --
+    const userLeftNotifications = (userCount, ws, users) => {
+      console.log('usercount as leaving: ', userCount)
+      userCount.type = 'userLeftNotification'
+      userCount.count = wss.clients.size
+      userCount.users = users
+      userCount.wsid = ws.id
+      userCount.id = uuidv1()
+      //need to broadcast the id of the user that left
+      broadcast(userCount)
+    }
+
+    saveUser = (data) => {
+      data.username = 'Anonymous'
+      const newuser = Object.assign({}, users)
+      newuser[data.wsid] = {
+        username: data.username
+      }
+      users = newuser
+      console.log(users)
     }
 
     // Create the WebSockets server
     const wss = new SocketServer({ server });
 
+    let users = {}
+
     // Set up a callback that will run when a client connects to the server
     // When a client connects they are assigned a socket, represented by
     // the ws parameter in the callback.
-    let users = {
-      count: 0
-    }
+
     wss.on('connection', (ws) => {
+      let userCount = {
+          count: wss.clients.size
+      }
       console.log('Client connected');
-      userJoinedNotifications(users)
-      console.log("users: ", users)
+      userJoinedNotifications(userCount, ws)
+      console.log("userCount: ", userCount)
 
       ws.on('message', (message) =>{
         const msg = JSON.parse(message)
-        msg.id = uuidv1()
         switch(msg.type) {
           case 'postMessage':
             console.log('recieved message: ', msg)
             msg.type = 'incomingMessage'
+            msg.wsid = ws.id
+            msg.id = uuidv1()
             break
           case 'postNotification':
             console.log('recieved notification: ', msg)
             msg.type = 'incomingNotification'
+            msg.wsid = ws.id
+            msg.id = uuidv1()
             break
           default:
             console.log(msg.type)
         }
         broadcast(msg)
+        console.log('broadcast: ', msg)
       })
       // Set up a callback for when a client closes the socket. This usually means they closed their browser.
       ws.on('close', () => {
+        userLeftNotifications(userCount, ws, users)
         console.log('Client disconnected')
-        userLeftNotifications(users)
     });
   });
