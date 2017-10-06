ca404eb2d40babf9cc5c0613a50b08303b06c45e
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-10-06 16:27:16 -0400

    clean up spacing

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx'
 import Notification from './Notification.jsx'
+import UserLeftNotification from './UserLeftNotification.jsx'
 
 class MessageList extends Component {
   constructor(props) {
@@ -8,22 +9,32 @@ class MessageList extends Component {
   }
 
 
-toRender (messages) {
+toRender = (messages) => {
   return messages.map((data) => {
     switch(data.type) {
       case 'incomingMessage':
+      console.log('msgList incoming message')
        return (
          <Message key={data.id} username={data.username} content={data.content}></Message>
        )
       case 'incomingNotification':
+      console.log('msgList incoming notification')
         return (
           <Notification key={data.id} content={data.content}></Notification>
         )
       case 'userJoinedNotification':
-      return (
+      console.log('msgList incoming user joined')
+        return (
           <Notification key={data.id} content={data.content}></Notification>
         )
+      case 'userLeftNotification':
+      console.log('msgList incoming user left')
+      debugger
+        return (
+          <UserLeftNotification key={data.id} username={data.username}></UserLeftNotification>
+        )
     }
+
   })
 }
 
